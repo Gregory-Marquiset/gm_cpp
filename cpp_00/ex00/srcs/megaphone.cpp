79f3e1747631bd37c7df45f7b835f7a547953970
gmarquis <gmarquis@student.42.fr>
2025-01-24 13:07:31 +0100

    cpp00_00

--- cpp_00/ex00/srcs/megaphone.cpp
+++ cpp_00/ex00/srcs/megaphone.cpp
@@ -1,7 +1,29 @@
-int	main(int argc, char **argv, char **env)
+# include <iostream>
+# include <string>
+
+int	main(int argc, char **argv)
 {
-	if (argc && argv && env)
-		return (0);
-	else
-		return (1);
+	int	j = 0;
+	int	trigger = 0;
+
+	if (argc > 1)
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			j = 0;
+			while (argv[i][j])
+			{
+				std::cout << static_cast<char>(std::toupper(argv[i][j]));
+				j++;
+			}
+		}
+		if (j > 1)
+		{
+			std::cout << std::endl;
+			trigger++;
+		}
+	}
+	if (argc == 1 || trigger == 0)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	return (0);
 }
