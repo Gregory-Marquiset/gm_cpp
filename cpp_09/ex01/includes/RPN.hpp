1ae06d24cfd3f5ffc57f1f0635b811a3470d2d02
gmarquis <gmarquis@student.42.fr>
2025-06-27 23:52:38 +0200

    cpp09ex01 fini

--- cpp_09/ex01/includes/RPN.hpp
+++ cpp_09/ex01/includes/RPN.hpp
@@ -7,11 +7,13 @@
 #include <list>
 #include <string>
 #include <iostream>
+#include <sstream>
 
 class	RPN
 {
 	private:
-		std::stack< float, std::list<float> > _Pile;
+		std::stack< int, std::list<int> > _Pile;
+		void	ResolveEquation(const std::string& Equation);
 	public:
 		RPN();
 		RPN(const RPN& Other);
@@ -23,7 +25,23 @@ class	RPN
 		{
 			const char*	what() const throw();
 		};
-		class	NumberTooHightException : public std::exception
+		class	NumberNotInRangeException : public std::exception
+		{
+			const char*	what() const throw();
+		};
+		class	NoOperandsException : public std::exception
+		{
+			const char*	what() const throw();
+		};
+		class	DivisionByZeroException : public std::exception
+		{
+			const char*	what() const throw();
+		};
+		class	InvalideTokenException : public std::exception
+		{
+			const char*	what() const throw();
+		};
+		class	InvalideRPNException : public std::exception
 		{
 			const char*	what() const throw();
 		};
--- cpp_09/ex01/srcs/RPN.cpp
+++ cpp_09/ex01/srcs/RPN.cpp
@@ -14,7 +14,7 @@ RPN::RPN(const std::string& Equation)
 {
 	if (Equation.empty())
 		throw(NoEquationException());
-	std::cout	<< "Open with: " << Equation << std::endl;
+	ResolveEquation(Equation);
 }
 
 RPN::~RPN()
@@ -32,7 +32,74 @@ const char*	RPN::NoEquationException::what() const throw()
 	return ("no equation to solve.");
 }
 
-const char*	RPN::NumberTooHightException::what() const throw()
+const char*	RPN::NumberNotInRangeException::what() const throw()
 {
-	return ("number greater than 10.");
+	return ("Positive integer less than 10 required.");
+}
+
+const char*	RPN::NoOperandsException::what() const throw()
+{
+	return ("not enough operands to apply the operator.");
+}
+
+const char*	RPN::DivisionByZeroException::what() const throw()
+{
+	return (" division by zero.");
+}
+
+const char*	RPN::InvalideTokenException::what() const throw()
+{
+	return ("invalide token.");
+}
+
+const char*	RPN::InvalideRPNException::what() const throw()
+{
+	return ("invalide RPN expression.");
+}
+
+void	RPN::ResolveEquation(const std::string& Equation)
+{
+	std::istringstream	Iss(Equation);
+	std::string			Token;
+	int					A, B, Result;
+
+	while (Iss >> Token)
+	{
+		if (Token == "+" || Token == "-" || Token == "/" || Token == "*")
+		{
+			if (_Pile.size() < 2)
+				throw(NoOperandsException());
+			
+			A = _Pile.top();
+			_Pile.pop();
+			B = _Pile.top();
+			_Pile.pop();
+			if (Token == "+")
+				Result = B + A;
+			else if (Token == "-")
+				Result = B - A;
+			else if (Token == "*")
+				Result = B * A;
+			else
+			{
+				if (A == 0)
+					throw(DivisionByZeroException());
+				Result = B / A;
+			}
+			_Pile.push(Result);
+		}
+		else
+		{
+			std::istringstream	conv(Token);
+			int					num;
+			if(!(conv >> num) || !(conv.eof()))
+				throw(InvalideTokenException());
+			if (num >= 10 || num < 0)
+				throw(NumberNotInRangeException());
+			_Pile.push(num);
+		}
+	}
+	if (_Pile.size() != 1)
+		throw(InvalideRPNException());
+	std::cout	<< _Pile.top() << std::endl;
 }
